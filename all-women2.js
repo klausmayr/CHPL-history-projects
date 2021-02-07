fdf3df9dbdc65440f3d4bc68c522f5ae7ef3f6d0
klausmayr <55473722+klausmayr@users.noreply.github.com>
2021-02-07 11:15:23 -0500

    Add files via upload

---
+++ all-women2.js
@@ -0,0 +1,8 @@
+var womanLayer = {"type":"FeatureCollection","features":[{"type":"Feature","id":"1","properties":{"name":"All Women"},
+    "geometry":{"type":"Polygon","coordinates":[[20,0],[0,0],[0,20],[20,20]]}},
+
+{"type":"Feature","id":"2","properties":{"name":"All Women Vert"},
+    "geometry":{"type":"Polygon","coordinates":[[20,20],[0,20],[0,50],[20,50]]}}
+    
+            ]
+            }
