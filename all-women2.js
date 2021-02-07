bb69c601b893e5b2bffc18015871f8bc794ac826
klausmayr <55473722+klausmayr@users.noreply.github.com>
2021-02-07 12:00:35 -0500

    Delete all-women2.js

--- all-women2.js
+++
@@ -1,8 +0,0 @@
-var womanLayer = {"type":"FeatureCollection","features":[{"type":"Feature","id":"1","properties":{"name":"All Women"},
-    "geometry":{"type":"Polygon","coordinates":[[20,0],[0,0],[0,20],[20,20]]}},
-
-{"type":"Feature","id":"2","properties":{"name":"All Women Vert"},
-    "geometry":{"type":"Polygon","coordinates":[[20,20],[0,20],[0,50],[20,50]]}}
-    
-            ]
-            }
