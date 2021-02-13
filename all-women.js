c91ffa9529031e422f1e94c6a2d45f980aa1762d
klausmayr <55473722+klausmayr@users.noreply.github.com>
2021-02-13 16:54:03 -0500

    Delete all-women.js

--- all-women.js
+++
@@ -1,6 +0,0 @@
-var womenData = {"type":"FeatureCollection","features":[
-        {"type":"Feature","id":"1","properties":{"name":"All Women"},"geometry":{"type":"Polygon","coordinates":[[[0,0],[20,20]]]},
-        {"type":"Feature","id":"2","properties":{"name":"All Women Vert"},"geometry":{"type":"Polygon","coordinates":[[[0,20],[20,50]]]}}
-    
-            ]
-            };
